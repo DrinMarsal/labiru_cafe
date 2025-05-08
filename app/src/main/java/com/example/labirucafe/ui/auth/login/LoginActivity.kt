475847363a4abe6fa48d1e6249032d55b5ac6bfd
Drin3304 <drinmarsal@gmail.com>
2025-05-08 15:26:00 +0700

    update

---
+++ app/src/main/java/com/example/labirucafe/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,96 @@
+package com.example.labirucafe.ui.auth.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.labirucafe.ui.main.MainActivity
+import com.example.labirucafe.databinding.ActivityLoginBinding
+//import com.example.labirucafe.ui.ViewModelFactory
+//import com.example.labirucafe.ui.auth.reset.forget.ForgetPasswordActivity
+//import com.example.labirucafe.ui.auth.signup.SignupActivity
+
+
+class LoginActivity : AppCompatActivity() {
+//    private val loginViewModel by viewModels<LoginViewModel> {
+//        ViewModelFactory.getInstance(this)
+//    }
+    private lateinit var binding: ActivityLoginBinding
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityLoginBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        setupAction()
+//        setupLoginObserver()
+//
+//        loginViewModel.authToken.observe(this){ token ->
+//            if(token.isNotEmpty()){
+//                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+//                finish()
+//            }
+//        }
+//
+//        binding.tvRedirect.setOnClickListener {
+//            val intent = Intent(this@LoginActivity, SignupActivity::class.java).apply {
+//                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//            }
+//            startActivity(intent)
+//            finish()
+//        }
+//
+//
+//        binding.tvForgotPassword.setOnClickListener {
+//            startActivity(Intent(this@LoginActivity, ForgetPasswordActivity::class.java))
+//        }
+//    }
+//
+//
+//    private fun setupAction() {
+//        binding.btnLogin.setOnClickListener {
+//            val email = binding.edLoginEmail.text.toString()
+//            val password = binding.edLoginPassword.text.toString()
+//            loginViewModel.loginUser(email, password)
+//
+//            loginViewModel.userLogin.observe(this) {
+//                val token = it.token.toString()
+//                loginViewModel.saveSession(token)
+//            }
+//        }
+//    }
+//
+//    private fun setupErrorDialog(message: String) {
+//        AlertDialog.Builder(this).apply {
+//            setTitle("Oops!")
+//            setMessage(message)
+//            setPositiveButton("OK", null)
+//            create()
+//            show()
+//        }
+//    }
+//
+//    private fun showLoading(isLoading: Boolean){
+//        if(isLoading) binding.progressIndicator.visibility = View.VISIBLE else binding.progressIndicator.visibility = View.GONE
+//    }
+//
+//    private fun showButton(isEnabled: Boolean){
+//        binding.btnLogin.isEnabled = !isEnabled
+//    }
+//
+//    private fun setupLoginObserver(){
+//        loginViewModel.isLoading.observe(this){
+//            showLoading(it)
+//            showButton(it)
+//        }
+//
+//        loginViewModel.responseLogin.observe(this) {
+//            setupErrorDialog(it)
+//        }
+//
+//    }
+
+}
